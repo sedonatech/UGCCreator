38e625cffdcf79f47545566b9cb10511d5980191
MichaelAmoabengVaulitions <michael.amoabeng2015@gmail.com>
2023-02-11 15:38:46 +0200

    Implemented feeds screen

---
+++ src/screens/app/feeds/components/FeedCard.tsx
@@ -0,0 +1,109 @@
+import React, { FC } from 'react';
+import { StyleSheet } from 'react-native';
+
+import { RADIUS_SMALL, SCREEN_WIDTH } from '../../../../theme/Layout';
+import {
+    BLACK, WHITE, BLACK_30, BRAND_BLUE, TRANSPARENT, WHITE_30
+} from '../../../../theme/Colors';
+import BackgroundImage from '../../../../components/BackgroundImage';
+import TemplateText from '../../../../components/TemplateText';
+import TemplateBox from '../../../../components/TemplateBox';
+import TemplateIcon from '../../../../components/TemplateIcon';
+
+interface Props {
+    image?: string | number;
+    title?: string;
+    shortDescription?: string;
+
+    subtitle?: string;
+    style?: any;
+
+    cardWidth?: number;
+    aspectRatio?: number;
+    slideInDelay?: number;
+
+    showGradient?: boolean;
+
+    showVideoButton?: boolean;
+
+    onPress?: () => void;
+}
+
+const FeedCard: FC<Props> = ({
+    image,
+    style,
+    shortDescription,
+    title,
+    cardWidth = SCREEN_WIDTH / 1.6,
+    aspectRatio = 1.5,
+    slideInDelay,
+    showGradient = true,
+    subtitle,
+    showVideoButton,
+    onPress,
+}) => (
+    <TemplateBox
+        fullGradient={showGradient}
+        alignItems="center"
+        justifyContent="center"
+        gradientColors={[BLACK_30, BLACK_30]}
+        borderRadius={RADIUS_SMALL}
+        width={cardWidth}
+        aspectRatio={aspectRatio}
+        style={style}
+        slideIn={slideInDelay !== undefined}
+        slideInDelay={slideInDelay}
+        backgroundColor={!image ? BRAND_BLUE : TRANSPARENT}
+        onPress={onPress}
+    >
+        {showVideoButton && (
+            <TemplateBox
+                height={40}
+                width={40}
+                absolute
+                top={10}
+                left={SCREEN_WIDTH - 100}
+                borderRadius={10}
+                backgroundColor={WHITE_30}
+                alignItems="center"
+                justifyContent="center"
+            >
+                <TemplateIcon name="videocam-outline" size={24} color={BLACK} />
+            </TemplateBox>
+        )}
+        {image && <BackgroundImage source={image} style={styles.image} width="100%" />}
+
+        <TemplateBox pAll={20}>
+            {/* @ts-ignore */}
+            <TemplateText color={WHITE} bold size={20} style={styles.text}>
+                {title}
+            </TemplateText>
+
+            {subtitle && (
+            // @ts-ignore
+                <TemplateText color={WHITE} italic size={10} style={styles.subtitle}>
+                    {subtitle}
+                </TemplateText>
+            )}
+            {/* @ts-ignore */}
+            <TemplateText color={WHITE} size={16} style={styles.text} numberOfLines={2}>
+                {shortDescription}
+            </TemplateText>
+        </TemplateBox>
+    </TemplateBox>
+);
+
+const styles = StyleSheet.create({
+    image: {
+        borderRadius: 10,
+        zIndex: -1,
+    },
+    text: {
+        marginBottom: 5,
+    },
+    subtitle: {
+        marginBottom: 20,
+    }
+});
+
+export default FeedCard;
