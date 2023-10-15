d41a49eea755fe93a7f5762ca26158797bb5bfc6
kndt <mnxx@protonmail.ch>
2023-10-15 18:16:16 +0100

    check all possible permissions results

--- src/hooks/imageUpload/useFirebaseSetStorage.js
+++ src/hooks/imageUpload/useFirebaseSetStorage.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
-import { Platform, Alert } from 'react-native';
+import { Alert } from 'react-native';
 import { get } from 'lodash';
 import storage from '@react-native-firebase/storage';
 import ImagePicker from 'react-native-image-crop-picker';
-import { request, PERMISSIONS } from 'react-native-permissions';
+import { request, PERMISSIONS, RESULTS } from 'react-native-permissions';
 
 import {
     options,
     optionsLandscapeMode,
     randomFileName,
 } from '../../Utils/ImageUpload';
+import { isIOS } from '../../Utils/Platform';
 
 const useFirebaseSetStorage = () => {
     const [progress, setProgress] = useState(0);
@@ -26,10 +27,6 @@ const useFirebaseSetStorage = () => {
         try {
             const path = get(response, 'path');
             const filename = get(response, 'filename');
-            // eslint-disable-next-line no-undef
-            const blob = new Blob([JSON.stringify('', null, 2)], {
-                type: 'image/jpeg',
-            });
             const isProgressPicture = filename || randomFileName();
             const imageName = isAvatar || isProgressPicture;
             const metadata = { customMetadata };
@@ -97,17 +94,46 @@ const useFirebaseSetStorage = () => {
 
                 if (err.code === 'E_NO_CAMERA_PERMISSION') {
                     let permissionStatus;
-                    request(Platform.OS === 'ios' ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA).then((result) => {
+                    request(isIOS ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA).then((result) => {
                         permissionStatus = result
                     });
 
-                    if (permissionStatus !== 'granted') {
-                        Alert.alert(
-                            'Camera permission',
-                            'You denied camera permission',
-                            [{ text: 'OK' }]
-                        );
-                        throw new Error('User has not granted camera permissions');
+                    __DEV__ && console.log("something")
+
+                    switch (permissionStatus) {
+                        case RESULTS.GRANTED:
+                            break;
+                        case RESULTS.BLOCKED:
+                            Alert.alert(
+                                'Camera permission',
+                                `You have ${permissionStatus} camera permission`,
+                                [{ text: 'OK' }]
+                            );
+                            break;
+                        case RESULTS.DENIED:
+                            Alert.alert(
+                                'Camera permission',
+                                `You have ${permissionStatus} camera permission`,
+                                [{ text: 'OK' }]
+                            );
+                            break;
+
+                        case RESULTS.LIMITED:
+                            Alert.alert(
+                                'Camera permission',
+                                `Your device camera is has ${permissionStatus} capabilities`,
+                                [{ text: 'OK' }]
+                            );
+                            break;
+                        case RESULTS.UNAVAILABLE:
+                            Alert.alert(
+                                'Camera permission',
+                                `Your device camera is ${permissionStatus}`,
+                                [{ text: 'OK' }]
+                            );
+                            break;
+                        default:
+                            break;
                     }
                 }
             });
