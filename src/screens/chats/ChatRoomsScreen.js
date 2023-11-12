4e683a3bb57079974ea07cef54d45009b9d2fa07
michaelamoabengvaulitions <michael.amoabeng2015@gmail.com>
2023-11-12 22:43:42 +0200

    Feature/Add search to chats

--- src/screens/chats/ChatRoomsScreen.js
+++ src/screens/chats/ChatRoomsScreen.js
@@ -1,11 +1,14 @@
-import { FlatList, StyleSheet, RefreshControl } from 'react-native';
-import React from 'react';
+import {
+    FlatList, StyleSheet, RefreshControl, KeyboardAvoidingView, StatusBar, View,
+} from 'react-native';
+import React, { useEffect, useMemo, useState } from 'react';
 import FastImage from 'react-native-fast-image';
 import PropTypes from 'prop-types';
 
+import Fuse from 'fuse.js';
 import useAuthContext from '../../hooks/auth/useAuthContext';
 import TemplateBox from '../../components/TemplateBox';
-import { WHITE } from '../../theme/Colors';
+import { BLACK, WHITE } from '../../theme/Colors';
 import {
     HEADER_MARGIN, SPACE_MEDIUM, WRAPPED_SCREEN_WIDTH, WRAPPER_MARGIN,
 } from '../../theme/Layout';
@@ -16,6 +19,10 @@ import { wp } from '../../Utils/getResponsiveSize';
 import useGetCreators from '../../hooks/brands/useGetCreators';
 import useGetBrands from '../../hooks/creators/useGetBrands';
 import { DEFAULT_CREATOR_WORK_SAMPLE_IMAGE } from '../../consts/content/Portfolio';
+import TemplateTextInput from '../../components/TemplateTextInput';
+import { SHADOW } from '../../theme/Shadow';
+import { isIOS } from '../../Utils/Platform';
+import TemplateSafeAreaView from '../../components/TemplateSafeAreaView';
 
 const ChatRoomsScreen = ({ navigation }) => {
     const { auth } = useAuthContext();
@@ -32,6 +39,34 @@ const ChatRoomsScreen = ({ navigation }) => {
         fetchingChatRooms,
     } = useChatsContext();
 
+    const [search, setSearch] = useState('');
+
+    const [searchResults, setSearchResults] = useState([]);
+
+    const fuseOptions = {
+        isCaseSensitive: false,
+        includeScore: true,
+        shouldSort: true,
+        minMatchCharLength: 1,
+        threshold: 0.4,
+        useExtendedSearch: true,
+        keys: [
+            'name',
+        ],
+    };
+
+    useEffect(() => {
+        if (!!search && chatRooms?.length) {
+            const fuse = new Fuse(chatRooms, fuseOptions);
+            const results = fuse.search(search).map(({ item }) => item);
+            setSearchResults(results);
+        }
+    }, [search]);
+
+    const searchedChatRooms = useMemo(() => (search?.length
+        ? searchResults
+        : chatRooms), [search, searchResults, chatRooms]);
+
     const getCreatorNameAndImage = (selectedId) => {
         if (isCreator && !creators?.length) return null;
 
@@ -45,6 +80,7 @@ const ChatRoomsScreen = ({ navigation }) => {
     const getBrandNameAndImage = (selectedId) => {
         if (!brands?.length && !isCreator) return null;
         const brand = brands?.find(({ id }) => id === selectedId);
+
         return {
             name: brand?.userName,
             image: brand?.image,
@@ -57,7 +93,7 @@ const ChatRoomsScreen = ({ navigation }) => {
         onPress,
     }) => (
         <TemplateBox
-            width={wp(WRAPPED_SCREEN_WIDTH) - wp(WRAPPER_MARGIN * 2)}
+            width={wp(WRAPPED_SCREEN_WIDTH) - wp(WRAPPER_MARGIN * 1.4)}
             borderRadius={wp(20)}
             pAll={wp(16)}
             selfCenter
@@ -85,71 +121,96 @@ const ChatRoomsScreen = ({ navigation }) => {
     };
 
     return (
-        <FlatList
-            data={chatRooms}
-            showsVerticalScrollIndicator={false}
-            keyExtractor={(item) => item?.id}
-            ListHeaderComponent={() => (
-                <TemplateBox
-                    mt={HEADER_MARGIN}
-                    alignItems="center"
-                    justifyContent="center"
-                    mh={WRAPPER_MARGIN}
-                >
-
-                    <TemplateText
-                        size={wp(18)}
-                        startCase
-                        bold
-                        center
+        <KeyboardAvoidingView
+            behavior={isIOS ? 'padding' : 'height'}
+            style={styles.mainContainer}
+        >
+            <StatusBar barStyle="dark-content" />
+            <FlatList
+                data={searchedChatRooms}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item) => item?.id}
+                ListHeaderComponent={(
+                    <>
+                        <TemplateBox
+                            mt={HEADER_MARGIN}
+                            alignItems="center"
+                            justifyContent="center"
+                            mh={WRAPPER_MARGIN}
+                        >
+                            <TemplateText
+                                size={wp(16)}
+                                startCase
+                                bold
+                                center
+                            >
+                                {`Continue your conversations with your ${isCreator ? 'brands' : 'creators'}`}
+                            </TemplateText>
+                            <TemplateBox height={WRAPPER_MARGIN} />
+                            <TemplateTextInput
+                                placeholder="Search"
+                                style={[styles.input, SHADOW('default', WHITE)]}
+                                value={search}
+                                onChangeText={(text) => setSearch(text)}
+                                autoCapitalize="none"
+                            />
+                            <TemplateBox height={WRAPPER_MARGIN} />
+                        </TemplateBox>
+                    </>
+                )}
+                renderItem={({ item }) => (
+                    <ChatRoomCard
+                        name={isCreator
+                            ? getBrandNameAndImage(item?.brandId)?.name
+                            : getCreatorNameAndImage(item?.creatorId)?.name}
+                        imageUrl={isCreator
+                            ? getBrandNameAndImage(item?.brandId)?.image
+                            : getCreatorNameAndImage(item?.creatorId)?.image}
+                        onPress={() => {
+                            navigation.navigate(CHATS, {
+                                chatRoomId: item?.id,
+                            });
+                        }}
+                    />
+                )}
+                ListEmptyComponent={() => (
+                    <TemplateBox
+                        mt={HEADER_MARGIN}
+                        alignItems="center"
+                        justifyContent="center"
+                        mh={WRAPPER_MARGIN}
                     >
-                        {`Continue your conversations with your ${isCreator ? 'brands' : 'creators'}`}
-                    </TemplateText>
-                    <TemplateBox height={WRAPPER_MARGIN} />
-                </TemplateBox>
-            )}
-            renderItem={({ item }) => (
-                <ChatRoomCard
-                    name={isCreator
-                        ? getBrandNameAndImage(item?.brandId)?.name
-                        : getCreatorNameAndImage(item?.creatorId)?.name}
-                    imageUrl={isCreator
-                        ? getBrandNameAndImage(item?.brandId)?.image
-                        : getCreatorNameAndImage(item?.creatorId)?.image}
-                    onPress={() => {
-                        navigation.navigate(CHATS, {
-                            chatRoomId: item?.id,
-                        });
-                    }}
-                />
-            )}
-            ListEmptyComponent={() => (
-                <TemplateBox
-                    mt={HEADER_MARGIN}
-                    alignItems="center"
-                    justifyContent="center"
-                    mh={WRAPPER_MARGIN}
-                >
-                    <TemplateText
-                        size={wp(16)}
-                        center
-                    >
-                        There are no conversations yet
-                    </TemplateText>
-                    <TemplateBox height={WRAPPER_MARGIN} />
-                </TemplateBox>
-            )}
-            style={styles.container}
-            contentContainerStyle={styles.contentContainer}
-            refreshControl={
-                <RefreshControl refreshing={fetchingChatRooms} onRefresh={fetchChatRooms} />
-            }
-        />
+                        <TemplateText
+                            size={wp(16)}
+                            center
+                        >
+                            There are no conversations yet
+                        </TemplateText>
+                        <TemplateBox height={WRAPPER_MARGIN} />
+                    </TemplateBox>
+                )}
+                ListFooterComponent={(
+                    <View style={styles.listFooter}>
+                        <TemplateSafeAreaView ios />
+                    </View>
+                )}
+                style={styles.container}
+                contentContainerStyle={styles.contentContainer}
+                refreshControl={
+                    <RefreshControl refreshing={fetchingChatRooms} onRefresh={fetchChatRooms} />
+                }
+                initialNumToRender={5}
+                onEndReachedThreshold={0.5}
+            />
+        </KeyboardAvoidingView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+    },
+    mainContainer: {
         flex: 1,
     },
     contentContainer: {
@@ -160,6 +221,17 @@ const styles = StyleSheet.create({
         height: wp(50),
         borderRadius: wp(10),
         marginRight: wp(20),
+    },
+    input: {
+        width: '100%',
+        height: wp(50),
+        borderRadius: wp(10),
+        paddingHorizontal: wp(12),
+        fontSize: wp(16),
+        color: BLACK,
+    },
+    listFooter: {
+        paddingBottom: wp(SPACE_MEDIUM),
     },
 });
 
