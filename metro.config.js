c5fce856ae851f242a60690bbfd4d22aec8edb9f
michaelamoabengvaulitions <michael.amoabeng2015@gmail.com>
2025-11-21 00:48:36 +0200

    Updated react native

--- metro.config.js
+++ metro.config.js
@@ -1,27 +1,10 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 /**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
  *
- * @format
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
+const config = {};
 
-module.exports = {
-    transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: true,
-            },
-        }),
-    },
-
-    server: {
-        rewriteRequestUrl: (url) => {
-            if (!url.endsWith('.bundle')) return url;
-
-            // https://github.com/facebook/react-native/issues/36794
-            // JavaScriptCore strips query strings, so try to re-add them with a best guess.
-            return `${url}?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true`;
-        }, // ...
-    },
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
--- src/components/TemplateTextInput.js
+++ src/components/TemplateTextInput.js
@@ -1,89 +1,70 @@
 import React, { useRef, useEffect } from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
-import Autocomplete from 'react-native-autocomplete-input';
-import {
-    BLACK, BLACK_SECONDARY, PRIMARY, WHITE,
-} from '../theme/Colors';
+import { BLACK, BLACK_SECONDARY, WHITE } from '../theme/Colors';
 
 const TemplateTextInput = ({
-    focus,
-    children,
-    disabled,
-    placeholderTextColor,
-    placeholderStyle,
-    value,
-    style,
-    autoComplete = false,
-    ...restProps
+  focus,
+  children,
+  disabled,
+  placeholderTextColor,
+  placeholderStyle,
+  value,
+  style,
+  autoComplete = false,
+  ...restProps
 }) => {
-    // Handle autofocus: create ref to input
-    const ref = useRef();
+  // Handle autofocus: create ref to input
+  const ref = useRef();
 
-    // Handle autofocus: if focus is true, call focus method
-    useEffect(() => {
-        if (focus) {
-            ref.current.focus();
-        }
-    }, [focus]);
+  // Handle autofocus: if focus is true, call focus method
+  useEffect(() => {
+    if (focus) {
+      ref.current.focus();
+    }
+  }, [focus]);
 
-    const Component = autoComplete ? Autocomplete : TextInput;
-    const autoCompleteProps = autoComplete
-        ? {
-            containerStyle: {
-                width: '100%',
-            },
-            inputContainerStyle: {
-                padding: 10,
-            },
-            listContainerStyle: {
-                border: 0,
-            },
-        }
-        : {};
-
-    return (
-        <Component
-            {...restProps}
-            ref={ref}
-            selectionColor={BLACK}
-            placeholderTextColor={placeholderTextColor || BLACK_SECONDARY}
-            value={value}
-            editable={!disabled}
-            style={[
-                styles.default,
-                restProps.style && restProps.style,
-                !value && placeholderStyle,
-                style,
-            ]}
-            {...autoCompleteProps}
-        />
-    );
+  return (
+    <TextInput
+      {...restProps}
+      ref={ref}
+      selectionColor={BLACK}
+      placeholderTextColor={placeholderTextColor || BLACK_SECONDARY}
+      value={value}
+      editable={!disabled}
+      style={[
+        styles.default,
+        restProps.style && restProps.style,
+        !value && placeholderStyle,
+        style,
+      ]}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    default: {
-        backgroundColor: WHITE,
-        color: BLACK,
-        fontSize: 15,
-        paddingHorizontal: 0, // Needed for android
-    },
+  default: {
+    backgroundColor: WHITE,
+    color: BLACK,
+    fontSize: 15,
+    paddingHorizontal: 0, // Needed for android
+  },
 });
 
 TemplateTextInput.propTypes = {
-    children: PropTypes.node,
-    focus: PropTypes.bool,
-    placeholderTextColor: PropTypes.string,
-    disabled: PropTypes.bool,
-    style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  children: PropTypes.node,
+  focus: PropTypes.bool,
+  placeholderTextColor: PropTypes.string,
+  disabled: PropTypes.bool,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
 };
 
 TemplateTextInput.defaultProps = {
-    children: null,
-    focus: false,
-    placeholderTextColor: null,
-    disabled: false,
-    style: null,
+  children: null,
+  focus: false,
+  placeholderTextColor: null,
+  disabled: false,
+  style: null,
 };
 
 export default TemplateTextInput;
