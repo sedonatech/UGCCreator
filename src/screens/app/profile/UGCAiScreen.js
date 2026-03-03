82a4fb0b6a0a3ff858dabdfe0250d553368b8772
Tosin Olaosebikan <tosebikan@gmail.com>
2026-03-03 13:13:09 +0000

    add translations

--- src/screens/app/profile/UGCAiScreen.js
+++ src/screens/app/profile/UGCAiScreen.js
@@ -3,51 +3,52 @@ import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import TemplateBox from '../../../components/TemplateBox';
 import TemplateText from '../../../components/TemplateText';
-import {
-
-    SCREEN_WIDTH, WRAPPED_SCREEN_WIDTH, WRAPPER_MARGIN,
-} from '../../../theme/Layout';
-import {
-    IOS_BLUE,
-    PAYWALL_PRIMARY_BACKGROUND,
-    WHITE,
-} from '../../../theme/Colors';
+import { HEADER_MARGIN, SCREEN_WIDTH, WRAPPED_SCREEN_WIDTH, WRAPPER_MARGIN } from '../../../theme/Layout';
+import { BLACK_20, IOS_BLUE, PAYWALL_PRIMARY_BACKGROUND, WHITE } from '../../../theme/Colors';
 import ScriptsSvg from '../../../../assets/svgs/ScriptsSvg';
 import SuggestorSvg from '../../../../assets/svgs/SuggestorSvg';
 import HooksSvg from '../../../../assets/svgs/HooksSvg';
 import {
     CONTENT_SUGGESTOR,
-    HOOKS_GENERATOR, RESULTS_HISTORY,
+    HOOKS_GENERATOR,
+    RESULTS_HISTORY,
     SCRIPTS_GENERATOR,
 } from '../../../navigation/ScreenNames';
 import Button from '../../../components/Button';
 import useTrackEvent from '../../../hooks/events/useTrackEvent';
+import { hp } from '../../../Utils/getResponsiveSize';
+import useTranslation from '../../../hooks/useTranslation';
 
 const UGCAiScreen = ({ navigation }) => {
+    const { t } = useTranslation();
+
     const creatorTools = [
         {
-            title: 'Scripts Generator',
-            description: 'This tool helps you generate scripts for your videos based on your project requirements.',
+            analyticsName: 'Scripts Generator',
+            title: t('creatorTools.ugcAiScreen.tools.scripts.title'),
+            description: t('creatorTools.ugcAiScreen.tools.scripts.description'),
             screen: SCRIPTS_GENERATOR,
             icon: 'scripts',
             type: 'scripts',
-            screenTitle: 'Create Engaging Scripts With AI in minutes',
+            screenTitle: t('creatorTools.ugcAiScreen.tools.scripts.screenTitle'),
         },
         {
-            title: 'Content Suggester',
-            description: 'This tool  suggests content for you to create based on your project requirements.',
+            analyticsName: 'Content Suggester',
+            title: t('creatorTools.ugcAiScreen.tools.contentSuggestion.title'),
+            description: t('creatorTools.ugcAiScreen.tools.contentSuggestion.description'),
             screen: CONTENT_SUGGESTOR,
             icon: 'suggestor',
             type: 'content suggestion',
-            screenTitle: 'Create Engaging Content With AI in minutes',
+            screenTitle: t('creatorTools.ugcAiScreen.tools.contentSuggestion.screenTitle'),
         },
         {
-            title: 'Hooks Generator',
-            description: 'This tool helps you generate hooks for your project.',
+            analyticsName: 'Hooks Generator',
+            title: t('creatorTools.ugcAiScreen.tools.hooks.title'),
+            description: t('creatorTools.ugcAiScreen.tools.hooks.description'),
             screen: HOOKS_GENERATOR,
             icon: 'hooks',
             type: 'hooks',
-            screenTitle: 'Create Engaging Hooks With AI in minutes',
+            screenTitle: t('creatorTools.ugcAiScreen.tools.hooks.screenTitle'),
         },
     ];
 
@@ -59,77 +60,64 @@ const UGCAiScreen = ({ navigation }) => {
     const { trackEvent } = useTrackEvent();
 
     return (
-        <ScrollView
-            style={styles.container}
-            contentContainerStyle={styles.contentContainer}
-        >
-            <TemplateBox
-                mh={WRAPPER_MARGIN}
-                alignItems="center"
-                justifyContent="center"
-            >
-                <TemplateText
-                    size={18}
-                    bold
-                    startCase
-                    center
-                >
-                    Explore our Creator Tools
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+            <TemplateBox mt={HEADER_MARGIN} mh={WRAPPER_MARGIN} alignItems="center" justifyContent="center">
+                <TemplateText size={18} bold startCase center>
+                    {t('creatorTools.ugcAiScreen.title')}
                 </TemplateText>
                 <TemplateBox mh={WRAPPER_MARGIN}>
-                    {
-                        creatorTools.map((item, index) => (
+                    {creatorTools.map((item, index) => (
+                        <TemplateBox
+                            row
+                            alignItems="center"
+                            backgroundColor={WHITE}
+                            borderRadius={16}
+                            pAll={20}
+                            width={WRAPPED_SCREEN_WIDTH}
+                            mt={WRAPPER_MARGIN}
+                            key={`${item.title}-${index}`}
+                            onPress={() => {
+                                navigation.navigate(SCRIPTS_GENERATOR, {
+                                    type: item.type,
+                                    title: item.screenTitle,
+                                });
+                                trackEvent('creator_tool_viewed', {
+                                    creator_tool_name: item.analyticsName,
+                                });
+                            }}
+                            borderWidth={1}
+                            borderColor={BLACK_20}
+                        >
+                            {iconMap[item.icon]}
+                            <TemplateBox width={16} />
                             <TemplateBox
-                                row
-                                alignItems="center"
-                                backgroundColor={WHITE}
-                                borderRadius={16}
-                                pAll={20}
-                                width={WRAPPED_SCREEN_WIDTH}
-                                mt={WRAPPER_MARGIN}
-                                key={`${item.title}-${index}`}
+                                width={SCREEN_WIDTH / 1.6}
                                 onPress={() => {
-                                    navigation.navigate(SCRIPTS_GENERATOR, {
+                                    navigation.navigate(item.screen, {
                                         type: item.type,
                                         title: item.screenTitle,
                                     });
                                     trackEvent('creator_tool_viewed', {
-                                        creator_tool_name: item.title,
+                                        creator_tool_name: item.analyticsName,
                                     });
                                 }}
                             >
-                                {iconMap[item.icon]}
-                                <TemplateBox width={16} />
-                                <TemplateBox
-                                    width={SCREEN_WIDTH / 1.6}
-                                    onPress={() => {
-                                        navigation.navigate(SCRIPTS_GENERATOR, {
-                                            type: item.type,
-                                            title: item.screenTitle,
-                                        });
-                                        trackEvent('creator_tool_viewed', {
-                                            creator_tool_name: item.title,
-                                        });
-                                    }}
-                                >
-                                    <TemplateText bold size={16}>{item.title}</TemplateText>
-                                    <TemplateBox height={10} />
-                                    <TemplateText size={13}>{item.description}</TemplateText>
-                                </TemplateBox>
+                                <TemplateText bold size={16}>
+                                    {item.title}
+                                </TemplateText>
+                                <TemplateBox height={10} />
+                                <TemplateText size={13}>{item.description}</TemplateText>
                             </TemplateBox>
-
-                        ))
-                    }
-
+                        </TemplateBox>
+                    ))}
                 </TemplateBox>
                 <Button
-                    title="View Results History"
+                    title={t('creatorTools.ugcAiScreen.viewResultsHistory')}
                     onPress={() => navigation.navigate(RESULTS_HISTORY)}
                     style={styles.button}
                     loading={false}
                 />
             </TemplateBox>
-
         </ScrollView>
     );
 };
@@ -137,14 +125,11 @@ const UGCAiScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: PAYWALL_PRIMARY_BACKGROUND,
-
+        backgroundColor: WHITE,
     },
     contentContainer: {
         flexGrow: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: PAYWALL_PRIMARY_BACKGROUND,
+        backgroundColor: WHITE,
     },
 
     button: {
@@ -153,6 +138,7 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         backgroundColor: IOS_BLUE,
         width: WRAPPED_SCREEN_WIDTH,
+        height: hp(45),
     },
 });
 export default UGCAiScreen;
